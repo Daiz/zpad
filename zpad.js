b6ec989083eb7418663f18a8ba429f31d184137e
Daiz <taneli.vatanen@gmail.com>
2014-01-23 10:43:07 +0200

    Change default and padWith to getter/setter functions with chaining

--- zpad.js
+++ zpad.js
@@ -1,12 +1,28 @@
 var zpad = function(n, m, c) {
-  if (!m) m = zpad.default;
-  if (!c) c = zpad.padWith;
+  if (!m) m = zpad.default$;
+  if (!c) c = zpad.padWith$;
   if (m < 1) m = 1;
   n = "" + n;
   while(n.length < m) n = c + n;
   return n;
 }
-zpad.default = 2;
-zpad.padWith = '0';
+zpad.default$ = 2;
+zpad.padWith$ = '0';
+zpad.default = function(a) {
+  if(a) {
+    zpad.default$ = a;
+    return zpad;
+  } else {
+    return zpad.default$;
+  }
+}
+zpad.padWith = function(c) {
+  if(c) {
+    zpad.padWith$ = c;
+    return zpad;
+  } else {
+    return zpad.padWith$;
+  }
+}
 
 module.exports = zpad;
