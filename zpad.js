d0a586a77da66a2ada65a7cf7ed813738f49f78c
Daiz <taneli.vatanen@gmail.com>
2014-01-23 10:51:13 +0200

    Change .padWith to .character

--- zpad.js
+++ zpad.js
@@ -1,13 +1,13 @@
 var zpad = function(n, m, c) {
   if (!m) m = zpad.default$;
-  if (!c) c = zpad.padWith$;
+  if (!c) c = zpad.character$;
   if (m < 1) m = 1;
   n = "" + n;
   while(n.length < m) n = c + n;
   return n;
 }
 zpad.default$ = 2;
-zpad.padWith$ = '0';
+zpad.character$ = '0';
 zpad.default = function(a) {
   if(a) {
     zpad.default$ = a;
@@ -16,12 +16,12 @@ zpad.default = function(a) {
     return zpad.default$;
   }
 }
-zpad.padWith = function(c) {
+zpad.character = function(c) {
   if(c) {
-    zpad.padWith$ = c;
+    zpad.character$ = c;
     return zpad;
   } else {
-    return zpad.padWith$;
+    return zpad.character$;
   }
 }
 
